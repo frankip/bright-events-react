7428d4162038a03f196280d9076cf795bb8ab97c
frank <gwadafrank@gmail.com>
2018-03-11 00:40:57 +0300

    [Feature ]  implement registration form

---
+++ src/components/authentication.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import Registration from "./registration";
+
+class Authentication extends Component {
+  render() {
+    return (
+    <div className="body">
+        <div className="intro">
+          <div>
+            <h1>
+              Welcome to<br />
+              <span>Bright Events</span>
+            </h1>
+            <p>
+              create and manage different types of events while making them
+              easily accessible to target markets
+            </p>
+          </div>
+        </div>
+      </div>
+      )
+  }
+}
+
+export default Authentication
--- src/components/registration.js
+++ src/components/registration.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-class Authentication extends Component {
+class Registration extends Component {
   render() {
     return (
-    <div className="body">
-        <div className="intro">
-            <div>
-                <h1>Welcome to<br/><span>Bright Events</span></h1>
-                <p>create and manage different types of events while making them easily accessible to target markets</p>
+        <div class="form">
+        <ul class="tab-group">
+            <li class="tab active"><a href="#signup">Sign Up</a></li>
+            <li class="tab"><a href="#login">Log In</a></li>
+        </ul>
+        <div class="tab-content">
+            <div id="signup">
+                <h3>Sign Up for Free</h3>
+                <form>
+                    <div class="top-row">
+                        <div class="field-wrap">
+                            <label> First Name<span class="req">*</span></label>
+                            <input type="text" required autocomplete="off" /> </div>
+                        <div class="field-wrap">
+                            <label> Last Name<span class="req">*</span></label>
+                            <input type="text" required autocomplete="off" /> </div>
+                    </div>
+                    <div class="field-wrap">
+                        <label> Email Address<span class="req">*</span> </label>
+                        <input type="email" required autocomplete="off" /> </div>
+                    <div class="field-wrap">
+                        <label> Set A Password<span class="req">*</span> </label>
+                        <input type="password" required autocomplete="off" /> </div> <a type="submit" class="button button-block" href="index.html">Get Started</a> </form>
             </div>
+            <div id="login">
+                <h3>Welcome Back!</h3>
+                <form action="index.html" method="post">
+                    <div class="field-wrap">
+                        <label> Email Address<span class="req">*</span> </label>
+                        <input type="email" required autocomplete="off" /> </div>
+                    <div class="field-wrap">
+                        <label> Password<span class="req">*</span> </label>
+                        <input type="password" required autocomplete="off" /> </div>
+                    <p class="forgot"><a href="#">Forgot Password?</a></p> <a class="button button-block" href="index.html">Log In</a> </form>
+            </div>
+        </div>
     </div>
 
-    </div>);
+    );
   }
 }
 
-export default Authentication
+export default Registration;
